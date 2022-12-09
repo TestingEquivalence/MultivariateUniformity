f38d055ebffb7a600a2bf2bf909c247ca902d159
Ostrovski <vladimir_ostrovski@web.de>
2022-12-09 16:38:42 +0100

    test 3d

--- tests/test 1 distance d3.R
+++ tests/test 1 distance d3.R
@@ -1,9 +1,9 @@
 source("distance.R")
 source("size.R")
 
-# case d=2 
-d=2
-n=50^2
+# case d=3 
+d=3
+n=10^3
 vx=getUniformSample(d,n)
 
 
@@ -15,4 +15,4 @@ f<-function(x){
 m=getMax(f,vx)
 
 distance(vx)
-integral.mc(f,d,m,10000)
+integral.mc(f,d,m,50000)
